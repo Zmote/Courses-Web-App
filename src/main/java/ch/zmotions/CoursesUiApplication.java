95d0614d5b7f3e82ece139f428bfc0f647bb282a
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2016-11-28 20:29:09 +0100

    first commit

---
+++ src/main/java/ch/zmotions/CoursesUiApplication.java
@@ -0,0 +1,12 @@
+package ch.zmotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesUiApplication.class, args);
+	}
+}
